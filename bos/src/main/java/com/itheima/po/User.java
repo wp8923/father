d89b064014307dc85317cec4288e8e7b208cbea2
Administrator <13039907869@163.com>
2018-01-22 16:42:18 +0800

    提交

---
+++ bos/src/main/java/com/itheima/po/User.java
@@ -0,0 +1,57 @@
+package com.itheima.po;
+
+
+public class User {
+
+  private long id;
+  private String username;
+  private java.sql.Date birthday;
+  private String sex;
+  private String address;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+
+  public java.sql.Date getBirthday() {
+    return birthday;
+  }
+
+  public void setBirthday(java.sql.Date birthday) {
+    this.birthday = birthday;
+  }
+
+
+  public String getSex() {
+    return sex;
+  }
+
+  public void setSex(String sex) {
+    this.sex = sex;
+  }
+
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+}
